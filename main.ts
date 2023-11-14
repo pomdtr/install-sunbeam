3f561025d0ad149c68a584239a084d0593a7ab3f
pomdtr <achille.lacoin@gmail.com>
2023-11-14 20:03:23 +0100

    add sunbeam install script

--- main.ts
+++ main.ts
@@ -1,20 +1,72 @@
-import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
-import data from "./data.json" assert { type: "json" };
+import shlex from "npm:shlex";
 
-const app = new Hono();
+const script = (tag: string) => `#!/bin/sh
+# This script installs sunbeam.
+#
+# Quick install: 'curl -sSf https://install-sunbeam.deno.dev | sh'
+# inspired by https://sshx.io/ install script
 
-app.get("/", (c) => c.text("Welcome to dinosaur API!"));
+set -eu
 
-app.get("/api/", (c) => c.json(data));
+tag=${shlex.quote(tag)}
+system=$(uname -s | tr '[:upper:]' '[:lower:]')
+arch=$(uname -m)
 
-app.get("/api/:dinosaur", (c) => {
-  const dinosaur = c.req.param("dinosaur").toLowerCase();
-  const found = data.find((item) => item.name.toLowerCase() === dinosaur);
-  if (found) {
-    return c.json(found);
-  } else {
-    return c.text("No dinosaurs found.");
+tempdir=$(mktemp -d)
+trap 'rm -rf $tempdir' EXIT
+
+url="https://github.com/pomdtr/sunbeam/releases/download/\${tag}/sunbeam-\${system}_\${arch}.tar.gz"
+printf "↯ Downloading sunbeam from %s\\n" "$url"
+http_code=$(curl -L "$url" -o "$tempdir/sunbeam.tar.gz" -w "%{http_code}")
+if [ "$http_code" -lt 200 ] || [ "$http_code" -gt 299 ]; then
+  printf "Error: Request had status code %s.\\n" "$http_code" 1>&2
+  cat "$tempdir/sunbeam.tar.gz" 1>&2
+  exit 1
+fi
+
+tar zxf "$tempdir/sunbeam.tar.gz" -C "$tempdir"
+printf "\\n↯ Adding sunbeam binary to /usr/local/bin\\n"
+if [ "$(id -u)" -ne 0 ]; then
+  sudo mv "$tempdir/sunbeam" /usr/local/bin/sunbeam
+else
+  mv "$tempdir/sunbeam" /usr/local/bin/sunbeam
+fi
+
+printf "\\n↯ Done! You can now run sunbeam.\\n"
+`;
+
+const githubToken = Deno.env.get("GITHUB_TOKEN")
+if (!githubToken) {
+  throw new Error("GITHUB_TOKEN environment variable is required")
+}
+
+async function installSunbeam(request: Request): Promise<Response> {
+  const url = new URL(request.url);
+  const params = new URLSearchParams(url.search);
+
+  let tag = params.get("tag");
+  if (!tag) {
+    const resp = await fetch("https://api.github.com/repos/pomdtr/sunbeam/releases", {
+      headers: {
+        "User-Agent": "install-sunbeam.deno.dev",
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": `Bearer ${githubToken}`
+      }
+    })
+    const releases = await resp.json()
+    if (releases.length == 0) {
+      return new Response("Not Found", {
+        status: 404,
+      });
+    }
+    tag = releases[0].tag_name
   }
-});
 
-Deno.serve(app.fetch);
+  return new Response(script(tag!), {
+    headers: {
+      "Content-Type": "application/x-shellscript",
+    },
+  });
+}
+
+Deno.serve(installSunbeam);
